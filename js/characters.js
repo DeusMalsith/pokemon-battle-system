785b4f81e42466d11b6a68fb60de81cdcff9158e
DeusMalsith <metalgear132@gmail.com>
2018-08-06 18:12:14 -0700

    added an array of pokemon with a functional attack feature outputting to console log

--- js/characters.js
+++ js/characters.js
@@ -18,12 +18,29 @@ let moves = {
 };
 
 class Pokemon {
-	constructor(health, moves, img) {
+	constructor(pokename, level, health, moves, img) {
+		this.pokename = pokename;
+		this.level = level;
 		this.health = health;
 		this.moves = moves;
 		this.img = img;
 	}
+	decrementHealth(damage) {
+		this.health -= damage;
+	}
+	attack(target, move) {
+		target.decrementHealth(move.damage);
+	}
 };
 
-let pikachu = new Pokemon(100, [moves['tackle'], moves['thundershock']], 'pikachu.png');
-let charmander = new Pokemon(100, [moves['scratch'], moves['ember']], 'charmander.png');
+pokemon = [];
+pokemon.push(new Pokemon('pikachu', 5, 100, [moves['tackle'], moves['thundershock']], 'pikachu.png'));
+pokemon.push(new Pokemon('charmander', 6, 100, [moves['scratch'], moves['ember']], 'charmander.png'));
+pokemon.push(new Pokemon('squirtle', 7, 100, [moves['tackle'], moves['thundershock']], 'squirtle.png'));
+pokemon.push(new Pokemon('evee', 5, 100, [moves['tackle'], moves['thundershock']], 'evee.png'));
+pokemon.push(new Pokemon('bulbasaur', 8, 100, [moves['tackle'], moves['thundershock']], 'bulbasaur.png'));
+pokemon.push(new Pokemon('ekans', 5, 100, [moves['tackle'], moves['thundershock']], 'ekans.png'));
+
+
+let player = pokemon[0];
+let enemy = pokemon[1];
--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 console.log('working');
 
-let turn = 0;
-
+function initGame() {
+	
+}
 
 
 function attack1() {
-	
+	player.attack(enemy, player.moves[0]);
 	console.log('attacked with first attack');
+	console.log(enemy.health);
 }
 
 function attack2() {
+	player.attack(enemy, player.moves[1]);
 	console.log('attacked with 2nd attack');
+	console.log(enemy.health);
 }
 
 document.getElementById('attack1').addEventListener('click', attack1);
