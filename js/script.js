152787c50b3e3cc9a44c1b7d477445255a4e0af8
DeusMalsith <metalgear132@gmail.com>
2018-08-10 12:19:52 -0700

    basic combat functionality with hit animations. Cursor bug was fixed.

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ function initGame() {
 	enemyPokemon = enemyParty[0];
 
 	showPokemon();
+	addListeners();
 }
 
 function showPokemon(){
@@ -29,6 +30,8 @@ function showPokemon(){
 	document.getElementById('pkmn-name').textContent = enemyPokemon.pokename;
 	document.getElementById('pkmnback-maxhp').textContent = playerPokemon.maxhealth
 	document.getElementById('pkmnback-hp').textContent = playerPokemon.health
+	document.getElementById('attack1').textContent = playerPokemon.moves[0].name;
+	document.getElementById('attack2').textContent = playerPokemon.moves[1].name;
 }
 
 initGame();
@@ -41,46 +44,99 @@ function itemButton() {
 
 }
 
+function fightButton() {
+	document.getElementById('b2').src = "./assets/img/pkmnbattle2.png";
+	document.getElementById('attackcancel').style.zIndex = '1';
+	document.getElementById('attack1').style.zIndex = '1';
+	document.getElementById('attack2').style.zIndex = '1';
+}
+
+function pkmnButton() {
+
+}
+
+function cancelButton() {
+	document.getElementById('attackcancel').style.zIndex = '-1';
+	document.getElementById('attack1').style.zIndex = '-1';
+	document.getElementById('attack2').style.zIndex = '-1';
+	document.getElementById('b2').src = "";
+
+}
+
 function attack1() {
 	playerPokemon.attack(enemyPokemon, playerPokemon.moves[0]);
+	document.getElementById('attackcancel').style.zIndex = '-1';
+	document.getElementById('attack1').style.zIndex = '-1';
+	document.getElementById('attack2').style.zIndex = '-1';
+	document.getElementById('b2').src = "";
+	document.getElementById('pkmn').style.animation = 'blink 0.15s 5';
+	setTimeout(function() {
+		document.getElementById('pkmn').style.animation = '';
+	}, 1000);
 	//console.log('attacked with first attack');
 	//console.log(enemyPokemon.health);
 	enemyPokemon.faint(enemyPokemon, enemyParty);
-	setTimeout(enemyAttack, 1000);
+	removeListeners();
+	setTimeout(function() {
+		enemyAttack();
+		addListeners();
+	}, 1000);
+
 	showPokemon();
 }
 
 function attack2() {
 	playerPokemon.attack(enemyPokemon, playerPokemon.moves[1]);
+	document.getElementById('attackcancel').style.zIndex = '-1';
+	document.getElementById('attack1').style.zIndex = '-1';
+	document.getElementById('attack2').style.zIndex = '-1';
+	document.getElementById('b2').src = "";
+	document.getElementById('pkmn').style.animation = 'blink 0.15s 5';
+	setTimeout(function() {
+		document.getElementById('pkmn').style.animation = '';
+	}, 1000);
 	//console.log('attacked with 2nd attack');
 	console.log(enemyPokemon.health);
 	enemyPokemon.faint(enemyPokemon, enemyParty);
-	setTimeout(enemyAttack, 1000);
+	removeListeners();
+	setTimeout(function () {
+		enemyAttack();
+		addListeners();
+	}, 1000);
 	showPokemon();
 }
 
 function enemyAttack() {
 	var attackMove = Math.floor(Math.random() * enemyPokemon.moves.length);
-	//console.log('attacked with',enemyPokemon.moves[attackMove].name);
+	console.log('attacked with',enemyPokemon.moves[attackMove].name);
 	enemyPokemon.attack(playerPokemon,enemyPokemon.moves[attackMove]);
+	document.getElementById('pkmnback').style.animation = 'blink 0.15s 5';
+	setTimeout(function() {
+		document.getElementById('pkmnback').style.animation = '';
+	}, 1000)
 	//console.log(playerPokemon.health);
 	showPokemon();
 	playerPokemon.faint(playerPokemon, playerParty);
 }
 
-// // Set in way where this works for enemy
-// function rest() {
-// 	playerPokemon.incrementHealth(playerPokemon, playerPokemon.moves[1]);
-// 	console.log('healed');
-// 	console.log(playerPokemon.health);
-// }
+function addListeners() {
+	document.getElementById('fight').addEventListener('click', fightButton);
+	document.getElementById('attackcancel').addEventListener('click', cancelButton);
+	document.getElementById('attack1').addEventListener('click', attack1);
+	document.getElementById('attack2').addEventListener('click', attack2);
+	// document.getElementById('items').addEventListener('click', potion);
+}
+
+function removeListeners() {
+	document.getElementById('fight').removeEventListener('click', fightButton);
+	document.getElementById('attackcancel').removeEventListener('click', cancelButton);
+	document.getElementById('attack1').removeEventListener('click', attack1);
+	document.getElementById('attack2').removeEventListener('click', attack2);
+	// document.getElementById('items').removeEventListener('click', potion);
+}
 
 function endGame() {
 	console.log('game over');
 }
 
-document.getElementById('attack1').addEventListener('click', attack1);
 
-document.getElementById('attack2').addEventListener('click', attack2);
-
-document.getElementById('items').addEventListener('click', potion);
